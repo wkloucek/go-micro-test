fb5a4923b4e5f2815e2d253360c5da3362444606
Willy Kloucek <wkloucek@owncloud.com>
2021-10-13 16:29:45 +0200

    add test example

---
+++ test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	httpServer "github.com/asim/go-micro/plugins/server/http/v3"
+	"github.com/asim/go-micro/v3"
+	"github.com/asim/go-micro/v3/server"
+)
+
+func main() {
+	srv := httpServer.NewServer(
+		server.Name("helloworld"),
+	)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`hello world`))
+	})
+
+	hd := srv.NewHandler(mux)
+
+	srv.Handle(hd)
+
+	service := micro.NewService(
+		micro.Server(srv),
+		micro.Address("[::]:65432"),
+	)
+	service.Init()
+	err := service.Run()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+}
